65022291eb4cc6df5b22324e0cdb8d3fd1c4ea63
Will Yu <yconquesty@gmail.com>
2024-05-03 20:14:15 +0200

    complete lab 6
    Finish `buffer.c`

--- testMultiCore/src/buffer.c
+++ testMultiCore/src/buffer.c
@@ -1,12 +1,33 @@
+static volatile int data[8]   = {1,2,3,4,5,6,7,8};
+static volatile int buffer[8] = {0,0,0,0,0,0,0,0};
+
+static volatile int flag = 0;
+static volatile int acc  = 0;
+
 char *s = "Success\n";
 char *f = "Failure\n";
 
-int program_thread0(){
-  return 1;
+int program_thread0()
+{
+    for (int i = 0; i < 8; ++i) {
+        buffer[i] = data[i];
+    }
+    flag = 1;
+    return 0;
 }
 
-int program_thread1(){
-  return 1;
+int program_thread1()
+{
+    while (!flag);
+
+    for (int i = 0; i < 8; ++i) {
+        acc += buffer[i];
+    }
+
+    if (acc == 1 + 2 + 3 + 4 + 5 + 6 + 7 + 8) {
+        return 0;
+    }
+    return 1;
 }
 
 
